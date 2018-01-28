179ad9ae55b160db52fd2903ac89f1fe0ce25ca7
Nyolczas <nyolczas@gmail.com>
2018-01-28 18:32:13 +0100

    basicCalculator

---
+++ bucky/src/bucky/BasicCalculator.java
@@ -0,0 +1,12 @@
+package bucky;
+import java.util.Scanner;
+
+
+public class BasicCalculator {
+	public static void main(String args[]) {
+		Scanner beir = new Scanner(System.in);
+		double elsoszam, masodikszam, valasz;
+		System.out.println("Írd be az elsõ számot!");
+		elsoszam = beir.nextDouble();
+	}
+}
